44b2857dea694e5ad07719cf4e0892fec380ca0f
Alexandre Osovski <74330383+AlexandreOsovski@users.noreply.github.com>
2023-01-18 18:02:59 -0300

    style: ajustes no estilo

---
+++ infinitycode_springboot/src/main/java/Models/CadastroUsuario.java
@@ -0,0 +1,47 @@
+package Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class CadastroUsuario {
+
+	@Id
+
+	@Column(name = "nome_completo")
+	private String nomeCompleto;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "senha")
+	private String senha;
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
---
+++ infinitycode_springboot/src/main/java/infinity_springboot/controller/Usuario.java
@@ -0,0 +1,23 @@
+package infinity_springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Usuario {
+
+	@RequestMapping("/login")
+	public String login() {
+		return "Usuario/login";
+	}
+
+	@RequestMapping("/cadastro")
+	public String CadastroUsuario() {
+		return "Usuario/cadastro";
+	}
+
+	@RequestMapping("/esquecisenha")
+	public String esqueciSenha() {
+		return "Usuario/esquecisenha";
+	}
+}
