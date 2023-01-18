aa68beabb6a81abdbd0bd71b7985140acbe0764a
Alexandre Osovski <74330383+AlexandreOsovski@users.noreply.github.com>
2023-01-18 13:30:01 -0300

    initial commit

---
+++ infinitycode_springboot/src/test/java/infinity_springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package infinity_springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
