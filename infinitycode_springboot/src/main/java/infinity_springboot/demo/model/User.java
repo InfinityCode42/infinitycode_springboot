c1e5f1fadeb2e5e6284b4a5c5774d320e203ad4e
Juan Fernandes <juanfernandesrrm@gmail.com>
2023-01-18 21:17:11 -0300

    feat: add users model class and repository

---
+++ infinitycode_springboot/src/main/java/infinity_springboot/demo/model/User.java
@@ -0,0 +1,18 @@
+package infinity_springboot.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String password;
+}
